28c0dec9ad3926e52d667564174bfeed871fabdd
IvoObr <ivo_obr@hotmail.com>
2018-12-28 18:39:15 +0200

    Heroku setup

--- server/server.js
+++ server/server.js
@@ -2,29 +2,56 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo-model');
 const {User} = require('./models/user-model');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
-app.post('/todo', (request, resonse) => {
+app.post('/todos', (request, response) => {
     let todo = new Todo({
         text: request.body.text
     });
 
     todo.save().then(todoDoc => {
-       resonse.send(todoDoc);
+        response.send(todoDoc);
     }, (error) => {
-        resonse.status(400).send(error);
+        response.status(400).send(error);
     });
 
 });
 
-const port = process.env.PORT || 3000;
+app.get('/todos', (request, response) => {
+    Todo.find().then(allTodos => {
+        response.send({allTodos});
+    }, (error) => {
+        response.status(400).send(error);
+    });
+});
+
+app.get('/todos/:id', (request, response) => {
+   let id = request.params.id;
+
+   if (!ObjectID.isValid(id)) {
+       return response.status(404).send();
+   }
+
+   Todo.findById(id).then(todoDoc => {
+       if (!todoDoc) {
+           return response.status(404).send();
+        }
+
+       response.send({todoDoc});
+
+   }).catch(error => {
+       response.status(400).send();
+   });
+});
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
