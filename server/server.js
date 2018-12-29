2a7d870371be39727a246176661876e539236d3e
IvoObr <ivo_obr@abv.bg>
2018-12-29 17:15:25 +0200

    DELETE and PATCH todos

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
+const _ = require('lodash');
 
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo-model');
@@ -13,13 +14,27 @@ const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
+function checkTodoAndRejectOrResolveCall(todoDoc, response) {
+    if (!todoDoc) {
+        return response.status(404).send();
+    }
+
+    response.send({todoDoc});
+}
+
+function rejectCallWithInvalidID(id, response) {
+    if (!ObjectID.isValid(id)) {
+        return response.status(404).send();
+    }
+}
+
 app.post('/todos', (request, response) => {
     let todo = new Todo({
         text: request.body.text
     });
 
     todo.save().then(todoDoc => {
-        response.send(todoDoc);
+        response.send({todoDoc});
     }, (error) => {
         response.status(400).send(error);
     });
@@ -36,18 +51,40 @@ app.get('/todos', (request, response) => {
 
 app.get('/todos/:id', (request, response) => {
    let id = request.params.id;
-
-   if (!ObjectID.isValid(id)) {
-       return response.status(404).send();
-   }
+   rejectCallWithInvalidID(id, response);
 
    Todo.findById(id).then(todoDoc => {
-       if (!todoDoc) {
-           return response.status(404).send();
-        }
+       checkTodoAndRejectOrResolveCall(todoDoc, response);
+   }).catch(error => {
+       response.status(400).send();
+   });
+});
 
-       response.send({todoDoc});
+app.delete('/todos/:id', (request, response) => {
+    let id = request.params.id;
+    rejectCallWithInvalidID(id, response);
 
+    Todo.findByIdAndDelete(id).then(todoDoc => {
+        checkTodoAndRejectOrResolveCall(todoDoc, response);
+    }).catch(error => {
+       response.status(400).send();
+    });
+});
+
+app.patch('/todos/:id', (request, response) => {
+    let id = request.params.id;
+    let body = _.pick(request.body, ['text', 'completed']);
+    rejectCallWithInvalidID(id, response);
+
+   if (_.isBoolean(body.completed) && body.completed) {
+       body.completedAt = new Date().getTime();
+   } else {
+       body.completedAt = null;
+       body.completed = false;
+   }
+
+   Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then(todoDoc => {
+       checkTodoAndRejectOrResolveCall(todoDoc, response);
    }).catch(error => {
        response.status(400).send();
    });
