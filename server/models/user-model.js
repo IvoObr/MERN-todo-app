7471b89050c0fa5e11488e6d5c81e645a9e0aa54
IvoObr <ivo_obr@hotmail.com>
2018-12-31 16:10:21 +0200

    POST '/user' request

--- server/models/user-model.js
+++ server/models/user-model.js
@@ -2,8 +2,9 @@
 
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const validator = require('validator');
 
-const userSchema = new Schema({
+const UserSchema = new Schema({
     name: {
         type: String,
         required: true,
@@ -13,15 +14,31 @@ const userSchema = new Schema({
     email: {
         type: String,
         required: true,
-        minlength: 5
+        minlength: 5,
+        unique: true,
+        validate: {
+            validator: (email) => {
+               return validator.isEmail(email);
+            },
+            message: '{VALUE} is not a valid email'
+        }
     },
     password: {
         type: String,
         required: true,
         minlength: 8
-    }
+    }, tokens: [{
+        access: {
+            type: String,
+            required: true
+        },
+        token: {
+            type: String,
+            required: true
+        }
+    }]
 });
 
-let User = mongoose.model('User', userSchema);
+let User = mongoose.model('User', UserSchema);
 
 module.exports = {User};
