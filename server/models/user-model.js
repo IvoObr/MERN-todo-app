249f46f148fccec9c524032f3a983e9c55ce250f
IvoObr <ivo_obr@abv.bg>
2019-01-02 19:42:50 +0200

    Add POST /users/login route

--- server/models/user-model.js
+++ server/models/user-model.js
@@ -93,6 +93,28 @@ UserSchema.statics.findByToken = function(token) {
 
 };
 
+UserSchema.statics.findByCredentials = function(email, password) {
+    let User = this;
+
+    return User.findOne({email}).then(user => {
+        if (!user) {
+            return new Promise((resolve, reject) => {
+                reject();
+            });
+        }
+
+        return new Promise((resolve, reject) => {
+            bcrypt.compare(password, user.password, (error, response) => {
+               if (response) {
+                   resolve(user);
+               } else {
+                   reject();
+               }
+            });
+        });
+    });
+};
+
 UserSchema.pre('save', function (next) {
     let user = this;
     const rounds = 17;
