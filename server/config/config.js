6115d9d09343e06467d383f3bcb1384ab64d2fb8
IvoObr <ivo_obr@abv.bg>
2019-01-03 16:11:25 +0200

    config for production

--- server/config/config.js
+++ server/config/config.js
@@ -1,2 +1,6 @@
-const config = require('./config.json');
-process.env.JWT_SECRET = config.JWT_SECRET;
+const env = process.env.NODE_ENV || 'development';
+
+if (env === 'development') {
+    const config = require('./config.json');
+    process.env.JWT_SECRET = config.JWT_SECRET;
+}
